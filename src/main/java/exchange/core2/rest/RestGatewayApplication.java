77f89e5b814546c68724dbc381ff7767779e433c
Spencer Neilan <spencer.neilan@gmail.com>
2022-06-25 14:19:18 -0500

    Added jar build task to exchange-gateway-rest

--- src/main/java/exchange/core2/rest/RestGatewayApplication.java
+++ src/main/java/exchange/core2/rest/RestGatewayApplication.java
@@ -17,6 +17,7 @@ package exchange.core2.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
